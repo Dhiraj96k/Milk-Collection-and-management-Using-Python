1f35b59568b517ade32d42238cc4e44cd6f69b16
Dhiraj Dhanaji Salunke <168630573+devlopwithme@users.noreply.github.com>
2024-10-09 19:11:12 +0530

    signup page for admin and user

--- demo.py
+++ demo.py
@@ -1,24 +1,18 @@
 import tkinter as tk
-from tkinter import messagebox
-
-# Function to be called when button is clicked
-def on_button_click():
-    messagebox.showinfo("Message", "Hello, Tkinter!")
 
 # Create the main application window
 root = tk.Tk()
-root.title("Tkinter Demo")
+root.title("Milk Collection and Management")
 
+# Automatically detect screen width and height
+screen_width = root.winfo_screenwidth()
+screen_height = root.winfo_screenheight()
+
+window_width = int(screen_width)
+window_height = int(screen_height)
 # Set the size of the window
-root.geometry("300x200")
+root.geometry(f"{window_width}x{window_height}")
 
-# Create a label widget
-label = tk.Label(root, text="Welcome to Tkinter!", font=("Helvetica", 14))
-label.pack(pady=20)
-
-# Create a button widget
-button = tk.Button(root, text="Click Me", command=on_button_click)
-button.pack(pady=10)
 
 # Start the Tkinter event loop
 root.mainloop()
